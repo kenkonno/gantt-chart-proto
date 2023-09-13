72bdbdd05d3dad02b1c6eacb4151030cf956043d
gunkatanaman <gunkatanaman@gmail.com>
2023-09-13 15:54:16 +0900

    稼働設定をユニットX工程にする

--- backend/models/db/operation_setting.go
+++ backend/models/db/operation_setting.go
@@ -6,7 +6,6 @@ type OperationSetting struct {
 	Id         *int32 `gorm:"primaryKey;autoIncrement"`
 	FacilityId int32  `gorm:"uniqueIndex: operation_setting_u_index"`
 	UnitId     int32  `gorm:"uniqueIndex: operation_setting_u_index"`
-	UserId     int32  `gorm:"uniqueIndex: operation_setting_u_index"`
 	ProcessId  int32  `gorm:"uniqueIndex: operation_setting_u_index"`
 	WorkHour   int32
 
