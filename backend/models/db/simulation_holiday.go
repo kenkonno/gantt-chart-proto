d5f3e9c7cc7d592ad0276e3f37a5c13a6a00b206
gunkatanaman <gunkatanaman@gmail.com>
2025-03-02 03:20:11 +0900

    simulationのmodel定義をembed形式に変更。 変更漏れが多々あったため、本番の方を修正すれば自動で反映されるようにした。 Repositoryの方もtable名を持たせているため、頑張ればembed形式に変更できるが今回は見送り。

--- backend/models/db/simulation_holiday.go
+++ backend/models/db/simulation_holiday.go
@@ -1,13 +1,5 @@
 package db
 
-import "time"
-
 type SimulationHoliday struct {
-	Id         *int32 `gorm:"primaryKey;autoIncrement"`
-	FacilityId int32
-	Name       string
-	Date       time.Time
-
-	CreatedAt time.Time
-	UpdatedAt int32
+	Holiday
 }
