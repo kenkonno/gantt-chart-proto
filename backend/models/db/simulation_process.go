82bcb768d05fb1ab48e8cbf1df53e88ebf93137f
gunkatanaman <gunkatanaman@gmail.com>
2025-03-02 03:20:11 +0900

    simulationのmodel定義をembed形式に変更。 変更漏れが多々あったため、本番の方を修正すれば自動で反映されるようにした。 Repositoryの方もtable名を持たせているため、頑張ればembed形式に変更できるが今回は見送り。

--- backend/models/db/simulation_process.go
+++ backend/models/db/simulation_process.go
@@ -1,13 +1,5 @@
 package db
 
-import "time"
-
 type SimulationProcess struct {
-	Id    *int32 `gorm:"primaryKey;autoIncrement"`
-	Name  string
-	Order int
-	Color string
-
-	CreatedAt time.Time
-	UpdatedAt int32
+	Process
 }
--- backend/models/db/simulation_ticket_user.go
+++ backend/models/db/simulation_ticket_user.go
@@ -1,13 +1,5 @@
 package db
 
-import "time"
-
 type SimulationTicketUser struct {
-	Id       *int32 `gorm:"primaryKey;autoIncrement"`
-	TicketId int32
-	UserId   int32
-
-	Order     int
-	CreatedAt time.Time
-	UpdatedAt int32
+	TicketUser
 }
